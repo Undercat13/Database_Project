c8b905a5c1780d525cbf4528536df2e562ef88a2
Undercat13 <48971142+Undercat13@users.noreply.github.com>
2022-04-12 19:26:40 -0400

    add_event

--- events/forms.py
+++ events/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Eventtbl
+from .models import Eventtbl, University, Rso, Review
 
 #create event form
 class EventForm(ModelForm):
@@ -8,21 +8,40 @@ class EventForm(ModelForm):
 		model = Eventtbl
 		fields = "__all__" # might not want all of the fields example:( 'event_id', 'event_email' ect)
 		labels = {
-			'event_id': '',
-			'event_email': '',
-			'date': '',
-			'event_category': '',
-			'event_description': '',
-			'event_phone': '',
-			'location_name': '',
+			'event_id': 'Event ID Number:',
+			'event_email': 'Contact Email:',
+			'date': 'Event Date:',
+			'event_category': 'Choose the type of event:',
+			'event_description': 'Event Descrition:',
+			'event_phone': 'Contact Phone Number:',
+			'location_name': 'Event Location:',
 		}
-
+		CATEGORY = [
+			('public', 'Public'),
+			('private', 'Private'),
+			('rso', 'Rso')
+		]
 		widgets = {
-			'event_id': forms.NumberInput(attrs={'class':'form-control', 'placeholder': 'Event Name'}),
-			'event_email': forms.EmailInput(attrs={'class':'form-control', 'placeholder': 'Contact Email'}),
-			'date': forms.DateInput(attrs={'class':'form-control', 'placeholder': 'Event Date MM/DD/YYYY HH:MM'}),
-			'event_category':  forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Event Type'}),
-			'event_description': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Event Descrition'}),
-			'event_phone':  forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Contact Phone Number'}),
+			'event_id': forms.NumberInput(attrs={'class':'form-control', 'placeholder': '#'}),
+			'event_email': forms.EmailInput(attrs={'class':'form-control', 'placeholder': 'contact@email.com'}),
+			'date': forms.DateInput(attrs={'class':'form-control', 'placeholder': 'MM/DD/YYYY HH:MM'}),
+			'event_category':  forms.Select(choices=CATEGORY, attrs={'class':'form-control'}),
+			'event_description': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Descrition'}),
+			'event_phone':  forms.NumberInput(attrs={'class':'form-control', 'placeholder': '### ### ####'}),
 			'location_name':  forms.TextInput(attrs={'class':'form-control','placeholder': 'Location'}),
 		}
+
+class UniversityForm(ModelForm):
+	class Meta:
+		model = University
+		fields = ['uni_id', 'uni_name', 'num_students', 'uni_description', 'uni_location'] # does not include addition of gallery photos for now.
+
+class RsoForm(ModelForm):
+	class Meta:
+		model = Rso
+		fields = "__all__"
+
+class ReviewForm(ModelForm):
+	class Meta:
+		model = Review
+		fields = "__all__"
